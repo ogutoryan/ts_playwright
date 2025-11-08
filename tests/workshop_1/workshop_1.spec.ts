991eb34600c9f3a2f976baa581905a99f84f5501
ogutoryan <olya.poddubnik@gmail.com>
2025-11-08 11:35:18 +0100

    main: workshop 1

---
+++ tests/workshop_1/workshop_1.spec.ts
@@ -0,0 +1,23 @@
+import { test } from "playwright/test";
+
+test("Basic navigation", async ({ page }) => {
+  await page.goto("https://github.com/");
+  await page.waitForTimeout(300);
+  await page.reload();
+});
+
+test('Interacting with GitLab web elements', async ({ page }) => {
+  await page.goto('https://gitlab.com/');
+  await page.click('#onetrust-accept-btn-handler');
+  await page.click('text=Get free trial');
+  await page.locator('[data-testid="new-user-first-name-field"]').fill('John');
+  await page.getByTestId('new-user-last-name-field').fill('Snow');
+ 
+});
+
+test.only('Using various locator methods', async ({page}) => {
+  await page.goto('https://gitlab.com/');
+  await page.getByText('Accept All Cookies').click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+})
